f71814cbce3df573b2a4b4d9dc4811617cd46154
David Rebbe <1187684+ic3man5@users.noreply.github.com>
2023-04-26 00:14:41 -0400

    rust reads the values now

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,42 @@
 slint::include_modules!();
 
-fn main() {
-    println!("Hello, world!");
-    MainWindow::new().unwrap().run().unwrap();
+#[derive(Debug, Clone)]
+struct Data {
+    pub age: f32,
+    pub age_of_death: f32,
+    pub age_of_retire: f32,
+    pub age_of_social: f32,
+    pub starting_balance: f32,
+    pub ss_monthly: f32,
+    pub contrib_monthly: f32,
+    pub yield_before_retire: f32,
+    pub yield_after_retire: f32,
+    pub inflation_rate: f32,
+    pub annual_income_post: f32,
+    pub annual_income_post_decay: f32,
+}
+
+fn main() -> Result<(), slint::PlatformError> {
+    let ui: MainWindow = MainWindow::new()?;
+    let ui_handle = ui.as_weak();
+    ui.on_calculate(move || {
+        println!("Hello World!");
+        let ui = ui_handle.unwrap();
+        let data = Data {
+            age: ui.get_current_age(),
+            age_of_death: ui.get_death_age(),
+            age_of_retire: ui.get_retirement_age(),
+            age_of_social: ui.get_social_security_age(),
+            starting_balance: ui.get_starting_balance(),
+            ss_monthly: ui.get_social_security_monthly(),
+            contrib_monthly: ui.get_monthly_contribution(),
+            yield_before_retire: ui.get_yield_before_retirement(),
+            yield_after_retire: ui.get_yield_after_retirement(),
+            inflation_rate: ui.get_inflation_rate(),
+            annual_income_post: ui.get_annual_income_post(),
+            annual_income_post_decay: ui.get_annual_income_post_decay(),
+        };
+        println!("{data:#?}");
+    });
+    ui.run()
 }
