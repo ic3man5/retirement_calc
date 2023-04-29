8814e8a086ba8478413dd0af49a2b0d3e1629db5
David Rebbe <1187684+ic3man5@users.noreply.github.com>
2023-04-28 21:28:10 -0400

    start of compound interest

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,36 @@
 slint::include_modules!();
 
+/// Calculates compound interest and returns the Accrued amount (Principal + Interest)
+/// 
+/// Parameters:
+///     p = Principal amount in dollars
+///     r = Annual nominal interest rate as a decimal
+///     n = number of compounding periods per unit of time (t)
+///     t = time in decimal years (6 months = 0.5 years)
+/// 
+/// Returns:
+///     Accrued amount (principal + interest)
+/// 
+/// Example:
+/// ```
+///     todo!();
+/// ```
+///
+pub fn compound_interest(p: f32, r: f32, n: f32, t: f32) -> f32 {
+    // A = P(1 + r/n)nt
+    // A = Accrued amount (principal + interest)
+    // P = Principal amount
+    // r = Annual nominal interest rate as a decimal
+    // R = Annual nominal interest rate as a percent
+    // r = R/100
+    // n = number of compounding periods per unit of time
+    // t = time in decimal years; e.g., 6 months is calculated as 0.5 years. Divide your partial year number of months by 12 to get the decimal years.
+    p*(1.0+r/n)*n*t
+    //principal * (1.0 + (rate/100.0 / period)) * period * years
+}
+
 #[derive(Debug, Clone)]
-struct Data {
+struct FinancialData {
     pub age: f32,
     pub age_of_death: f32,
     pub age_of_retire: f32,
@@ -16,13 +45,35 @@ struct Data {
     pub annual_income_post_decay: f32,
 }
 
+#[derive(Debug, Clone)]
+pub struct TimeStatistics {
+    pub yrs_life_remaining: f32,
+    pub yrs_until_retirement: f32,
+    pub yrs_until_ss: f32,
+    pub yrs_of_retirement: f32,
+    pub yrs_of_ss: f32,
+}
+
+impl FinancialData {
+    pub fn get_time_stats(&self) -> TimeStatistics {
+        TimeStatistics {
+            yrs_life_remaining: self.age_of_death - self.age,
+            yrs_until_retirement: self.age_of_retire - self.age,
+            yrs_until_ss: self.age_of_social - self.age,
+            yrs_of_retirement: self.age_of_death - self.age_of_retire,
+            yrs_of_ss: self.age_of_death - self.age_of_social,
+        }
+    }
+}
+
+
 fn main() -> Result<(), slint::PlatformError> {
     let ui: MainWindow = MainWindow::new()?;
     let ui_handle = ui.as_weak();
     ui.on_calculate(move || {
         println!("Hello World!");
         let ui = ui_handle.unwrap();
-        let data = Data {
+        let data = FinancialData {
             age: ui.get_current_age(),
             age_of_death: ui.get_death_age(),
             age_of_retire: ui.get_retirement_age(),
@@ -37,6 +88,11 @@ fn main() -> Result<(), slint::PlatformError> {
             annual_income_post_decay: ui.get_annual_income_post_decay(),
         };
         println!("{data:#?}");
+        let stats = data.get_time_stats();
+        println!("Time Statistics: {:#?}", stats);
+        let accurred_amount = compound_interest(data.starting_balance, data.yield_before_retire/100.0, 12.0, stats.yrs_of_retirement);
+        println!("{accurred_amount}");
+        
     });
     ui.run()
 }
